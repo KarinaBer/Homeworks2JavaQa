c7e267251339cd5ce297e96c5d356289579e2611
KarinaBer <112567394+KarinaBer@users.noreply.github.com>
2022-09-22 15:40:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(10_800);
+        int miles = service.calculate(10_000);
         System.out.println(miles);
     }
 }
